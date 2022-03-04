c49ea401d3356c84cbf88198345635a6e9d26f1e
ferrerooo <ferrerooo@email.com>
2022-03-04 00:36:20 -0800

    0303

---
+++ Binary Search/33_SearchinRotatedSortedArray.cs
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array/submissions/
+
+public class Solution33 {
+    public int Search(int[] nums, int target) {
+        
+        int start = 0;
+        int end = nums.Length - 1;
+        
+        while (end - start > 1) {
+            
+            int mid = start + (end - start) / 2;
+            
+            if (nums[start] < nums[mid]) {
+                if (target >= nums[start] && target <= nums[mid]) {
+                    end = mid;
+                    continue;
+                } else {
+                    start = mid;
+                    continue;
+                }
+            } else {
+                if (target >= nums[mid] && target <= nums[end]) {
+                    start = mid;
+                    continue;
+                } else {
+                    end = mid;
+                    continue;
+                }
+            }
+        }
+        
+        if (nums[start] == target)
+            return start;
+        else if (nums[end] == target)
+            return end;
+        else
+            return -1;
+    }
+}
---
+++ Binary Search/81_SearchinRotatedSortedArrayII.cs
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+
+public class Solution81 {
+    public bool Search(int[] nums, int target) {
+        
+        return Search(nums, 0, nums.Length - 1, target);
+        
+    }
+    
+    private static bool Search(int[] nums, int start, int end, int target) {
+        
+        if (end - start <= 1) {
+            return nums[start] == target || nums[end] == target;
+        }
+        
+        int mid = start + (end - start) / 2;
+        
+        if (nums[mid] < nums[end]) {
+            
+            if (nums[mid] <= target && target <= nums[end]) {
+                return Search(nums, mid, end, target);
+            } else {
+                return Search(nums, start, mid, target);
+            }            
+        }
+        
+        if (nums[start] < nums[mid]) {
+            
+            if (nums[start] <= target && target <= nums[mid]) {
+                return Search(nums, start, mid, target);
+            } else {
+                return Search(nums, mid, end, target);
+            } 
+        }
+        
+        return Search(nums, start, mid, target) || Search(nums, mid, end, target);
+    }
+}
