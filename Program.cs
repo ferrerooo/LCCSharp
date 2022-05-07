ae52a593148ba4ac080717ee4613d4808768f86d
ferrerooo <ferrerooo@gmail.com>
2022-05-07 10:23:52 -0700

    148

--- Program.cs
+++ Program.cs
@@ -6,10 +6,22 @@ namespace LC
     {
         static void Main(string[] args)
         {
-            Solution82 sol = new Solution82();
-            ListNode head = new ListNode(1);
-            sol.DeleteDuplicates(head);
-            Console.WriteLine("aab");
+            Solution148 sol = new Solution148();
+            ListNode n1 = new ListNode(4);
+            ListNode n2 = new ListNode(1);
+            //ListNode n3 = new ListNode(2);
+            //ListNode n4 = new ListNode(3);
+
+            n1.next = n2;
+            //n2.next = n3;
+            //n3.next = n4;
+
+            var result = sol.SortList(n1);
+
+            while (result != null) {
+                Console.WriteLine(result.val);
+                result = result.next;
+            }
         }
     }
 }
