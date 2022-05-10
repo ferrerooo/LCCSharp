a8ac006a4ed14a118ce876328fef7974648f354e
ferrerooo <ferrerooo@gmail.com>
2022-05-10 13:45:51 -0700

    55

--- Dynamic Programming/55_JumpGame.cs
+++ Dynamic Programming/55_JumpGame.cs
@@ -22,3 +22,36 @@ public class Solution55 {
     }
 }
 
+/*
+// another time do this problem. different from above solution.
+
+public class Solution {
+    public bool CanJump(int[] nums) {
+        
+        if (nums == null || nums.Length == 0)
+            return false;
+        
+        bool[] dp = new bool[nums.Length];
+        
+        dp[0] = true;
+        
+        for (int i = 1; i < nums.Length; i++) {
+            
+            for (int j = i - 1; j >= 0; j--) {
+                
+                if (j + nums[j] >= i) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+            
+            if (dp[i] == false)
+                return false;
+        }
+        
+        return true;
+    }
+}
+
+*/
+
