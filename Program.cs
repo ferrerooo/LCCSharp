3733767e14542e64ea968c700ff9ce773f949bee
ferrerooo <ferrerooo@gmail.com>
2022-10-28 13:34:19 -0700

    update

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@ public class Program
     public static void Main()
     {
         Console.WriteLine("hello");
+
+        Racecar818 rc = new Racecar818();
+        Console.WriteLine(0);
+        int steps = rc.Racecar(5);
+        Console.WriteLine(steps);
     }
 }
 
